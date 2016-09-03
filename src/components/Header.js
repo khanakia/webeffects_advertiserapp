e2dcf0d0d6cf83734fa7e3c56503d568b7a50210
webbio-developer <developer@webbio.nl>
2016-09-03 19:36:31 +0530

    Fonts Included, Password Chnage and User Profile Edit Functionality #1

--- src/components/Header.js
+++ src/components/Header.js
@@ -78,23 +78,23 @@ export default class Header extends Component {
 									<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-cog" aria-hidden="true"></span> <span className="caret"></span></a>
 									<ul className="dropdown-menu">
 										<li className="dropdown-header">Organization</li>
-										<li><Link to="organization">Organizations</Link></li>
+										<li><Link to="organization"><i className="fa fa-sitemap"></i> Organizations</Link></li>
 
 										{(this.props.current_org.permissions.org_can_update ||  this.props.current_org.permissions.can_manage_people_companies)
-											? <li><Link to="organization/companies">Company</Link></li> : ''}
+											? <li><Link to="organization/companies"><i className="fa fa-building"></i> Company</Link></li> : ''}
 										{(this.props.current_org.permissions.org_can_update ||  this.props.current_org.permissions.can_manage_people_companies)
-											? <li><Link to="organization/peoples">Peoples</Link></li> : ''}
+											? <li><Link to="organization/peoples"><i className="fa fa-users"></i> Peoples</Link></li> : ''}
 
 										<li role="separator" className="divider"></li>
 										<li className="dropdown-header">Settings</li>
 										{/*<li><Link to="settings/general">General</Link></li>*/}
-										<li><Link to="settings/template">Template</Link></li>
-										<li><Link to="settings/tags">Tags</Link></li>
+										<li><Link to="settings/template"><i className="fa fa-users"></i> Template</Link></li>
+										<li><Link to="settings/tags"><i className="fa fa-tags"></i> Tags</Link></li>
 
 										<li role="separator" className="divider"></li>
 										<li className="dropdown-header">Billing / Subscription</li>
-										<li><Link to="billing">Billing</Link></li>
-										<li><Link to="subscription">Subscription</Link></li>
+										<li><Link to="billing"><i className="fa fa-money"></i> Billing</Link></li>
+										<li><Link to="subscription"><i className="fa fa-subscript"></i> Subscription</Link></li>
 									</ul>
 								</li>
 								<li className="dropdown">
@@ -107,6 +107,7 @@ export default class Header extends Component {
 											<div className="useravtarCt">
 												<a className="user-avtar-link" href="javascript:void(0);" title="Change Avtar" onClick={(e)=> this.uploadUserProfileImage(e)}>
 													<div className="user-avtar" style={avtarStyle}></div>
+													<i className="fa fa-image"></i>
 												</a>
 											</div>
 											<div className="user_title text-center">{this.props.current_user.first_name + ' ' + this.props.current_user.last_name}</div>
@@ -116,7 +117,7 @@ export default class Header extends Component {
 										<li><Link to="organization"><i className="fa fa-check-square-o"></i> My Tasks</Link></li>
 										{/*<li><Link to="dashboard"><i className="fa fa-user"></i> Profile & Account</Link></li>*/}
 										<li><Link to="#" className="a_edit_my_detail" onClick={(e)=> this.updateProfile(e)} ><i className="fa fa-pencil"></i> Edit My Details</Link></li>
-										<li><Link to="#" className="change_password" onClick={(e) => this.changePassword(e)} ><i className="fa fa-pencil"></i> Change Password</Link></li>
+										<li><Link to="#" className="change_password" onClick={(e) => this.changePassword(e)} ><i className="fa fa-lock"></i> Change Password</Link></li>
 										<li role="separator" className="divider"></li>
 										<li><a href="#" onClick={(e) => {this.logout(e)}}><i className="fa fa-sign-out"></i>Logout</a></li>
 									</ul>
